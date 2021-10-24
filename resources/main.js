0f008578fec7d847cbe9e229bc46ecc4d6a54fa1
kingsley Diala <65650594+KingsleyDiala@users.noreply.github.com>
2021-10-24 18:52:25 +0200

    Add files via upload

---
+++ resources/main.js
@@ -0,0 +1,46 @@
+
+
+// Grab the HTML elements
+const navMenu = document.getElementsByClassName('nav_links')[0],
+open = document.getElementById('open'),
+close = document.getElementById('close'),
+show = document.getElementsByClassName('show')[0]
+
+close.style.display = 'none'
+navMenu.classList.remove('show')
+
+// Toggle Menu Open
+if (open) {
+  open.addEventListener('click', () => {
+    navMenu.classList.add('show')
+    close.style.display = 'block'
+    open.style.display = 'none'
+  })
+}
+
+
+// Close Menu
+if (close) {
+  close.addEventListener('click', () => {
+    navMenu.classList.remove('show')
+    open.style.display = 'block'
+    close.style.display = 'none'
+  })
+}
+
+// Close Menu if link is click on
+
+// Get the links
+const links = document.querySelectorAll('.link')
+
+function linkAction() {
+  navMenu.classList.remove('show')
+  close.style.display = 'none'
+  open.style.display = 'block'
+}
+
+links.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+
