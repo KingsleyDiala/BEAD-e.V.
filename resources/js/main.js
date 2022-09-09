027aaf85048dd9e6e9b45489b8bffed31301272c
kingsley Diala <donbuski2@gmail.com>
2022-09-09 10:16:25 +0200

    clean up

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,46 +1,50 @@
-
-
 // Grab the HTML elements
-const navMenu = document.getElementsByClassName('nav_links')[0],
-open = document.getElementById('open'),
-close = document.getElementById('close'),
-show = document.getElementsByClassName('show')[0]
+const navMenu = document.getElementsByClassName("nav_links")[0],
+  open = document.getElementById("open"),
+  close = document.getElementById("close"),
+  show = document.getElementsByClassName("show")[0];
 
-close.style.display = 'none'
-navMenu.classList.remove('show')
+close.style.display = "none";
+navMenu.classList.remove("show");
 
 // Toggle Menu Open
 if (open) {
-  open.addEventListener('click', () => {
-    navMenu.classList.add('show')
-    close.style.display = 'block'
-    open.style.display = 'none'
-  })
+  open.addEventListener("click", () => {
+    navMenu.classList.add("show");
+    close.style.display = "block";
+    open.style.display = "none";
+  });
 }
-
 
 // Close Menu
 if (close) {
-  close.addEventListener('click', () => {
-    navMenu.classList.remove('show')
-    open.style.display = 'block'
-    close.style.display = 'none'
-  })
+  close.addEventListener("click", () => {
+    navMenu.classList.remove("show");
+    open.style.display = "block";
+    close.style.display = "none";
+  });
 }
 
 // Close Menu if link is click on
 
 // Get the links
-const links = document.querySelectorAll('.link')
+const links = document.querySelectorAll(".link");
 
 function linkAction() {
-  navMenu.classList.remove('show')
-  close.style.display = 'none'
-  open.style.display = 'block'
+  navMenu.classList.remove("show");
+  close.style.display = "none";
+  open.style.display = "block";
 }
 
-links.forEach(n => n.addEventListener('click', linkAction))
+links.forEach((n) => n.addEventListener("click", linkAction));
 
+// HIGHLIGHT LINK ON ACTIVE PAGE
 
-
+let current = 0;
+for (var i = 0; i < document.links.length; i++) {
+  if (document.links[i].href === document.URL) {
+    current = i;
+  }
+}
+document.links[current].className = "current";
 
